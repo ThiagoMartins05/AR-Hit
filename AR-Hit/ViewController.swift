d27bac93388d9c31489a05249939117d84408bbc
thiago <thiagod.smartins@icloud.com>
2020-11-03 16:18:07 -0400

    initial setting, adding a neewView when tapping the startButton

--- AR-Hit/ViewController.swift
+++ AR-Hit/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    }
+    
+    func addNode(){
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        node.position = SCNVector3(0, 0, -1)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    
+    @objc func handleTap(sender: UITapGestureRecognizer){
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty {
+            print("didn't touch anything")
+        } else {
+            let results = hitTest.first!
+            let geometry = results.node.geometry
+            print(geometry as Any)
+        }
+    }
 }
 
